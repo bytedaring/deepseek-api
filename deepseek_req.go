c74f22ee59c30ca2d683099f1497cf4284de5f00
xiaoxiwang <daring@outlook.com>
2025-03-06 13:42:52 +0800

    go 1.22.3 -> 1.19

--- deepseek_req.go
+++ deepseek_req.go
@@ -1,6 +1,9 @@
 package deepseek_api
 
-import "errors"
+import (
+	"errors"
+	"strings"
+)
 
 const (
 	RESPONSE_FORMAT_TEXT        = "text"
@@ -78,75 +81,89 @@ func NewDeepSeekChatRequest(messages []DeepSeekMessage, model string) *DeepSeekC
 	}
 }
 
+type multiError []error
+
+func (m multiError) Error() string {
+	var errStrings []string
+	for _, err := range m {
+		errStrings = append(errStrings, err.Error())
+	}
+	return strings.Join(errStrings, "; ")
+}
+
 func (dsr *DeepSeekChatRequest) DeepSeekRequest() error {
-	var err error
+	var errs []error
 
 	if len(dsr.Messages) < 1 {
-		err = errors.Join(err, errors.New("messages must be at least one"))
+		errs = append(errs, errors.New("messages must be at least one"))
 	}
 
 	if dsr.Model == "" {
-		err = errors.Join(err, errors.New("model must be set"))
+		errs = append(errs, errors.New("model must be set"))
 	}
 
 	if dsr.FrequencyPenalty < -2 || dsr.FrequencyPenalty > 2 {
-		err = errors.Join(err, errors.New("frequency_penalty must be between -2 and 2"))
+		errs = append(errs, errors.New("frequency_penalty must be between -2 and 2"))
 	}
 
 	if dsr.MaxTokens < 1 || dsr.MaxTokens > 8192 {
-		err = errors.Join(err, errors.New("max_tokens must be between 1 and 8192"))
+		errs = append(errs, errors.New("max_tokens must be between 1 and 8192"))
 	}
 
 	if dsr.PresencePenalty < -2 || dsr.PresencePenalty > 2 {
-		err = errors.Join(err, errors.New("presence_penalty must be between -2 and 2"))
+		errs = append(errs, errors.New("presence_penalty must be between -2 and 2"))
 	}
 
 	if dsr.ResponseFormat.Type != RESPONSE_FORMAT_TEXT && dsr.ResponseFormat.Type != RESPONSE_FORMAT_JSON_OBJECT {
-		err = errors.Join(err, errors.New("response_format.type must be text or json_object"))
+		errs = append(errs, errors.New("response_format.type must be text or json_object"))
 	}
 
 	if len(dsr.Stop) > 16 {
-		err = errors.Join(err, errors.New("stop must be less than 16 lists"))
+		errs = append(errs, errors.New("stop must be less than 16 lists"))
 	}
 
 	if dsr.Stream {
 		if dsr.StreamOptions == nil {
-			err = errors.Join(err, errors.New("stream_options must be set when stream is true"))
+			errs = append(errs, errors.New("stream_options must be set when stream is true"))
 		}
 	} else {
 		if dsr.StreamOptions != nil {
-			err = errors.Join(err, errors.New("stream_options must be nil when stream is false"))
+			errs = append(errs, errors.New("stream_options must be nil when stream is false"))
 		}
 	}
 
 	if dsr.Temperature < 0 || dsr.Temperature > 2 {
-		err = errors.Join(err, errors.New("temperature must be between 0 and 2"))
+		errs = append(errs, errors.New("temperature must be between 0 and 2"))
 	}
 
 	if dsr.TopP < 0 || dsr.TopP > 1 {
-		err = errors.Join(err, errors.New("top_p must be between 0 and 1"))
+		errs = append(errs, errors.New("top_p must be between 0 and 1"))
 	}
 
 	switch dsr.ToolChoice {
 	case TOOL_CHOICE_NONE, TOOL_CHOICE_AUTO:
 	case TOOL_CHOICE_REQUIRED:
 		if len(dsr.Tools) < 1 {
-			err = errors.Join(err, errors.New("tools must be defined when tool_choice is required"))
+			errs = append(errs, errors.New("tools must be defined when tool_choice is required"))
 		}
 	default:
-		err = errors.Join(err, errors.New("tool_choice must be one of none, auto, or required"))
+		errs = append(errs, errors.New("tool_choice must be one of none, auto, or required"))
 	}
 
 	if dsr.TopLogprobs != nil {
 		if !dsr.Logprobs {
-			err = errors.Join(err, errors.New("top_logprobs must be defined when logprobs is true"))
+			errs = append(errs, errors.New("top_logprobs must be defined when logprobs is true"))
 		}
 		if *dsr.TopLogprobs < 0 || *dsr.TopLogprobs > 20 {
-			err = errors.Join(err, errors.New("top_logprobs must be between 0 and 20"))
+			errs = append(errs, errors.New("top_logprobs must be between 0 and 20"))
 		}
 	}
 
-	return err
+	if len(errs) > 0 {
+		return multiError(errs)
+	}
+	return nil
+
 }
 
 func (dsr *DeepSeekChatRequest) StreamModel() bool {
@@ -188,55 +205,62 @@ func NewDeepSeekCompletionsRequest(model string, prompt string) *DeepSeekComplet
 }
 
 func (dsr *DeepSeekCompletionsRequest) DeepSeekRequest() error {
-	var err error
+	// var err error
+	var errs []error
 
 	if dsr.Model == "" {
-		err = errors.Join(err, errors.New("model must be set"))
+		// errs = append(errs, errors.New("model must be set"))
+		errs = append(errs, errors.New("model must be set"))
 	}
 
 	if dsr.Prompt == "" {
-		err = errors.Join(err, errors.New("prompt must be set"))
+		// errs = append(errs, errors.New("prompt must be set"))
+		errs = append(errs, errors.New("prompt must be set"))
 	}
 
 	if dsr.FrequencyPenalty < -2 || dsr.FrequencyPenalty > 2 {
-		err = errors.Join(err, errors.New("frequency_penalty must be between -2 and 2"))
+		// errs = append(errs, errors.New("frequency_penalty must be between -2 and 2"))
+		errs = append(errs, errors.New("frequency_penalty must be between -2 and 2"))
 	}
 
 	if dsr.Logprobs < 0 || dsr.Logprobs > 20 {
-		err = errors.Join(err, errors.New("logprobs must be between 0 and 20"))
+		errs = append(errs, errors.New("logprobs must be between 0 and 20"))
 	}
 
 	if dsr.MaxTokens < 1 {
-		err = errors.Join(err, errors.New("max_tokens must be greater than 0"))
+		errs = append(errs, errors.New("max_tokens must be greater than 0"))
 	}
 
 	if dsr.PresencePenalty < -2 || dsr.PresencePenalty > 2 {
-		err = errors.Join(err, errors.New("presence_penalty must be between -2 and 2"))
+		errs = append(errs, errors.New("presence_penalty must be between -2 and 2"))
 	}
 
 	if len(dsr.Stop) > 16 {
-		err = errors.Join(err, errors.New("stop must be less than 16 lists"))
+		errs = append(errs, errors.New("stop must be less than 16 lists"))
 	}
 
 	if dsr.Stream {
 		if dsr.StreamOptions == nil {
-			err = errors.Join(err, errors.New("stream_options must be set when stream is true"))
+			errs = append(errs, errors.New("stream_options must be set when stream is true"))
 		}
 	} else {
 		if dsr.StreamOptions != nil {
-			err = errors.Join(err, errors.New("stream_options must be nil when stream is false"))
+			errs = append(errs, errors.New("stream_options must be nil when stream is false"))
 		}
 	}
 
 	if dsr.Temperature < 0 || dsr.Temperature > 2 {
-		err = errors.Join(err, errors.New("temperature must be between 0 and 2"))
+		errs = append(errs, errors.New("temperature must be between 0 and 2"))
 	}
 
 	if dsr.TopP < 0 || dsr.TopP > 1 {
-		err = errors.Join(err, errors.New("top_p must be between 0 and 1"))
+		errs = append(errs, errors.New("top_p must be between 0 and 1"))
 	}
 
-	return err
+	if len(errs) > 0 {
+		return multiError(errs)
+	}
+	return nil
 }
 
 func (dsr *DeepSeekCompletionsRequest) StreamModel() bool {
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/ZSLTChenXiYin/deepseek-api
 
-go 1.22.3
+go 1.19
